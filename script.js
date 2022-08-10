0cb8fb8010127bbfe555aede49f3780ef0ba644f
Gene Robles <eugenegrobles@gmail.com>
2022-08-10 23:18:37 +0800

    Declare initial variables and functions

---
+++ script.js
@@ -0,0 +1,17 @@
+let playerChoice = window.prompt("Choose your weapon:");
+let computerChoice = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice() {
+    return computerChoice[Math.floor(Math.random()*3)];
+}
+
+const playerSelection = playerChoice.toLowerCase;
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+
+}
+
+function game() {
+    
+}
