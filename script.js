1ffedb201a9ff26eb0776e1cea6045c88141a199
Gene Robles <eugenegrobles@gmail.com>
2022-08-11 14:02:08 +0800

    Declare the variables 'playerPoint' and 'computerPoint'. Update the functions playRound() and game()

--- script.js
+++ script.js
@@ -5,13 +5,51 @@ function getComputerChoice() {
     return computerChoice[Math.floor(Math.random()*3)];
 }
 
-const playerSelection = playerChoice.toLowerCase;
+const playerSelection = playerChoice.toLowerCase();
 const computerSelection = getComputerChoice();
+let playerPoint;
+let computerPoint;
 
 function playRound(playerSelection, computerSelection) {
-
+   if (playerSelection == computerSelection) {
+    return "Draw!";
+   } 
+   else if (playerSelection=='rock' && computerSelection=='scissors') {
+    playerPoint++;
+    return "You win! Rock beats Scissors";
+   }
+   else if (playerSelection=='scissors' && computerSelection=='paper') {
+    playerPoint++;
+    return "You win! Scissors beats Paper";
+   }
+   else if (playerSelection=='paper' && computerSelection=='rock') {
+    playerPoint++;
+    return "You win! Paper beats Rock";
+   }
+   else if (playerSelection=='scissors' && computerSelection=='rock') {
+    computerPoint++;
+    return "You lose! Rock beats Scissors";
+   }
+   else if (playerSelection=='paper' && computerSelection=='scissors') {
+    computerPoint++;
+    return "You lose! Scissors beats Paper";
+   }
+   else if (playerSelection=='rock' && computerSelection=='paper') {
+    computerPoint++;
+    return "You lose! Paper beats Rock";
+   }
 }
 
 function game() {
-    
+   for (let i = 0; i < 5; i++) {
+    playerPoint = i;
+    computerPoint = i;
+    playRound(playerSelection, computerSelection);
+    if (playerPoint === 5) {
+        return "You win!!";
+    }
+    else {
+        return "Computer wins";
+    }
+   } 
 }
